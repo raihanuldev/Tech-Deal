59fc151061ed6dfc4b3407a770b65a2763c37f20
raihanuldev <rihanthedev@gmail.com>
2024-04-22 04:35:28 +0600

    Navbar Done✔️

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import { NextPage } from "next";
 import Link from "next/link";
 import Image from "next/image";
+import { FaCartPlus } from "react-icons/fa6";
 
 const Navbar: NextPage = () => {
   return (
-    <header className="border-b border-black bg-[#ECECEC] md:px-10">
+    <header className="border-b border-black bg-[#382778] md:px-10 text-white">
       <div className="navbar">
         {/* logo */}
         <div className="navbar-start">
-        <div className="dropdown md:hidden">
+          <div className="dropdown md:hidden">
             <label tabIndex={0} className="btn btn-ghost btn-circle">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -42,7 +43,7 @@ const Navbar: NextPage = () => {
               </li>
             </ul>
           </div>
-        <Link href={"/"} className="font-bold text-2xl lg:text-2xl">
+          <Link href={"/"} className="font-bold text-2xl lg:text-2xl">
             <span className="text-red-600">Teach</span> Deal
           </Link>
         </div>
@@ -61,19 +62,33 @@ const Navbar: NextPage = () => {
                 <Link href="/blogs">Blog</Link>
               </li>
               <li>
-                <Link className="text-[#F07224]" href="/blogs">Dashbord</Link>
+                <Link className="text-[#F07224]" href="/blogs">
+                  Dashbord
+                </Link>
               </li>
             </ul>
           </div>
         </div>
         {/* img and cart  */}
         <div className="navbar-end">
+          <div className="">
+            
+            
+            <div className="badge">1</div>   
+            <FaCartPlus className="w-16 h-9 mr-3 text-red-400">
+            </FaCartPlus>
+          </div>
           <div className="avatar">
             <div className="w-11 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-              <Image src='https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg' className="rounded-full" alt="img" layout="fill" unoptimized />
+              <Image
+                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                className="rounded-full"
+                alt="img"
+                layout="fill"
+                unoptimized
+              />
             </div>
           </div>
-          
         </div>
       </div>
     </header>
