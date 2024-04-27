60692794f95eb83afea7dc8eadb27780bd53af24
raihanuldev <rihanthedev@gmail.com>
2024-04-28 05:09:43 +0600

    Blogs page defined

---
+++ src/pages/blogs/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NextPage } from 'next';
+
+const Blogs: NextPage = () => {
+  return (
+    <div>
+      <h1>Hello Next.js Page!</h1>
+    </div>
+  );
+};
+
+export default Blogs;
