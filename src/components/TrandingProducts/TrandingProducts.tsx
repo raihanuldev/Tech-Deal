0ac0a15291c583027642f9f036ba2ad2ed0bceec
raihanuldev <rihanthedev@gmail.com>
2024-05-15 00:49:55 +0600

    Tranding Products Issues Resiolved

--- src/components/TrandingProducts/TrandingProducts.tsx
+++ src/components/TrandingProducts/TrandingProducts.tsx
@@ -3,7 +3,7 @@ import { Key, useEffect, useState } from "react";
 import { productInterface } from "@/interface/ProductInterface";
 import ProductCard from "../ProductCard";
 
-const TrandingProducts: NextPage<{ products: productInterface }> = () => {
+const TrandingProducts: NextPage = () => {
   const [products, setProducts] = useState([]);
   console.log(products);
 
@@ -17,31 +17,10 @@ const TrandingProducts: NextPage<{ products: productInterface }> = () => {
       <h1 className="text-3xl font-bold my-4">Our Tranding Products</h1>
       <div className="grid md:grid-cols-4 space-y-2">
         {
-            products.map((product,index) => <ProductCard key={index} product={product}/>).splice(4,8)
+            products.map((product,index) => <ProductCard key={index} product={product}/>).splice(12,16)
         }
       </div>
     </>
   );
 };
 export default TrandingProducts;
-
-// export const getStaticProps:GetStaticProps = async () => {
-//     try {
-//       // const res = await fetch('https://tech-deal-backend-o5ta.vercel.app/products');
-//       const res = await fetch('http://localhost:5000/products');
-//       const data: productInterface[] = await res.json();
-//       console.log(data);
-//       return {
-//         props: {
-//           products: data
-//         }
-//       };
-//     } catch (error) {
-//       console.error('Error fetching products:', error);
-//       return {
-//         props: {
-//           products: []
-//         }
-//       };
-//     }
-//   };
