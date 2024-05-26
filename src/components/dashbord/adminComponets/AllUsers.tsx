4a9d2104d875c19852e94d461c5d3440c97228a7
raihanuldev <rihanthedev@gmail.com>
2024-05-26 17:44:21 +0600

    checked admin and Seller Dashbord Feauture

--- src/components/dashbord/adminComponets/AllUsers.tsx
+++ src/components/dashbord/adminComponets/AllUsers.tsx
@@ -5,7 +5,7 @@ const AllUsers = () => {
     const [users,setUsers] = useState([])
 
     useEffect(()=>{
-       fetch('http://localhost:5000/api/v1/syestem-users')
+       fetch('http://localhost:5000/users')
        .then(res=>res.json())
        .then(data=>setUsers(data))
     },[])
